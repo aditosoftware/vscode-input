41e078e04c71fb82bce734c13e19b0c64f8972d1
Fabian Adler <f.adler@adito.de>
2024-06-24 09:59:18 +0200

    refactor: remove DialogValues.confirmation
    chore: update version

--- src/DialogValues.ts
+++ src/DialogValues.ts
@@ -10,11 +10,6 @@ export class DialogValues {
   uri?: vscode.Uri;
 
   /**
-   * If there was a confirmation, then the result will be saved here.
-   */
-  confirmation?: boolean;
-
-  /**
    * The values that were given by the user.
    * The key is the input, which was used to get the value.
    * The value is always stored as a string[], even if it is only a string.
@@ -28,19 +23,17 @@ export class DialogValues {
    * @param result - the result given by the user
    */
   addValue(inputName: string, result: string | string[] | boolean): void {
-    if (typeof result === "boolean") {
-      this.confirmation = result;
-    } else {
-      // create the elements for storing it in the input values
-      const elements: string[] = [];
-      if (typeof result === "string") {
-        elements.push(result);
-      } else if (Array.isArray(result)) {
-        result.forEach((pResult) => elements.push(pResult));
-      }
-
-      // and store it there
-      this.inputValues.set(inputName, elements);
+    // create the elements for storing it in the input values
+    const elements: string[] = [];
+    if (typeof result === "string") {
+      elements.push(result);
+    } else if (Array.isArray(result)) {
+      result.forEach((pResult) => elements.push(pResult));
+    } else if (typeof result === "boolean") {
+      elements.push(result.toString());
     }
+
+    // and store it there
+    this.inputValues.set(inputName, elements);
   }
 }
